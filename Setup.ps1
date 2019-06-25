885c21b31c2b84d19fa4d49e19d154ff5dda32fb
tdmarchetta <tdmarchetta@gmail.com>
2019-06-25 12:46:34 -0700

    Change dir to Get-ChildItem

--- Setup.ps1
+++ Setup.ps1
@@ -10,12 +10,12 @@ Copy-Item -Path "$currentlocation\CTW_TXT_Add.ps1" -Destination 'C:\TXTAdder\'
 Copy-Item -Path "$currentlocation\CTW_TXT_Delete.bat" -Destination 'C:\TXTAdder\'
 Copy-Item -Path "$currentlocation\CTW_TXT_Delete.ps1" -Destination 'C:\TXTAdder\'
 
-###### Gets the API Key from the User.
+# Gets the API Key from the User.
 $getAPIKey = Read-Host -Prompt 'Dynu API Key, Please'
 $getAPIKey | Add-Content -Path "C:\TXTAdder\DynuAPIKey.txt"
 
-###### These PowerShell scripts are not signed...
+# These PowerShell scripts are not signed...
 Set-ExecutionPolicy Bypass
 
 # Unblock all files in TXTAdder
-dir C:\TXTAdder\ | Unblock-File
+Get-ChildItem C:\TXTAdder\ | Unblock-File
