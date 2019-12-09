88af558dd0a955e76b3c3b9f97ac9e81e7bf780c
tdmarchetta <tdmarchetta@gmail.com>
2019-12-09 15:18:23 -0800

    Clean up Setup

--- Setup.ps1
+++ Setup.ps1
@@ -1,19 +1,25 @@
-﻿# PowerShell To Setting up Scripts to talk to Certify The Web.
+﻿# PowerShell To Setting up Scripts to talk to Certify The Web & Dynu.
+# Version 1.0.4
 # Author: Taylor D. Marchetta
 
+# Variables
 $currentlocation = Get-Location
-New-Item -Path "C:\" -Name "TXTAdder" -ItemType Directory
+$TXTAdder = "C:\TXTAdder\"
+
+# Check for 'C:\crsadmin' folder.
+# '.trim' will remove leading and trailing spaces and leading and trailing line breaks
+if (!(test-path $TXTAdder.trim())) {
+
+    # Makes new folder for crsadmin and/or TXTAdder: C:\crsadmin\TXTAdder
+    New-Item -Path "$TXTAdder" -ItemType Directory
+}
 
 # Copy file(s) from the location where "Setup.ps1" is at.
-Copy-Item -Path "$currentlocation\CTW_TXT_Add.bat" -Destination 'C:\TXTAdder\'
-Copy-Item -Path "$currentlocation\CTW_TXT_Add.ps1" -Destination 'C:\TXTAdder\'
-Copy-Item -Path "$currentlocation\CTW_TXT_Delete.bat" -Destination 'C:\TXTAdder\'
-Copy-Item -Path "$currentlocation\CTW_TXT_Delete.ps1" -Destination 'C:\TXTAdder\'
-Copy-Item -Path "$currentlocation\RDGateway_Update.ps1" -Destination 'C:\TXTAdder\'
+Copy-Item -Path "$currentlocation\CTW_*.*" -Destination '$TXTAdder' -Recurse
 
 # $getAPIKey | Add-Content -Path 
 $getAPIKey = Read-Host -Prompt 'Dynu API Key, Please'
-$getAPIKey | Add-Content -Path "C:\TXTAdder\DynuAPIKey.txt"
+$getAPIKey | Add-Content -Path "$TXTAdder\DynuAPIKey.txt"
 
 # These PowerShell scripts are not signed...
 Set-ExecutionPolicy Bypass
