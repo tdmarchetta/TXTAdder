bf6ca717069693c9e7520101ac3c9c18230cfa0e
tdmarchetta <tdmarchetta@gmail.com>
2020-12-19 21:02:04 -0800

    Add TLS 1.2 Support

--- CTW_TXT_Add.ps1
+++ CTW_TXT_Add.ps1
@@ -1,52 +1,55 @@
 ﻿# TXTAdder
 # PowerShell To Adding TXT record with Certify the Web
-# Version 1.0.3
+# Version 1.0.4
 # Author: Taylor D. Marchetta
-# Huge thanks to James Crissman and Kyle Grey.
+# Huge thanks to Kyle Grey.
 
 # String(s) from the BAT file.
 param (
     [string]$zone,  #Domain Name
     [string]$id,    #Let's Encrypt nodeName - _acme-challenge.yourdomain.com
-    [string]$value  #HashValue for Let's Encrypt
+    [string]$value  #HashValue from Let's Encrypt
     )
+    
+# Force the use of TLS 1.2    
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Check for 'C:\TXTAdder\DynuAPIKey.txt' file.
 if (Test-Path C:\TXTAdder\DynuAPIKey.txt) {
 
-# Removes "IX HashValue" in wildcards.
-$zone = $zone -replace "'\*.',"
+    # Removes "IX HashValue" in wildcards.
+    $zone = $zone -replace "'\*.',"
 
-# Gets the API Key from the file "DynuAPIKey.txt".
-$APIKey = Get-Content -Path 'C:\TXTAdder\DynuAPIKey.txt'
+    # Gets the API Key from the file "DynuAPIKey.txt".
+    $APIKey = Get-Content -Path 'C:\TXTAdder\DynuAPIKey.txt'
 
-# GET Request to Dynu.com - This will get the ID of the domain.
-$getdomaindata = Invoke-RestMethod -Method GET -Uri ‘https://api.dynu.com/v2/dns/’ -ContentType ‘application/json’ -Headers @{ “Api-Key” = $APIKey }
+    # GET Request to Dynu.com - This will get the ID of the domain.
+    $getdomaindata = Invoke-RestMethod -Method GET -Uri ‘https://api.dynu.com/v2/dns/’ -ContentType ‘application/json’ -Headers @{ “Api-Key” = $APIKey }
 
-# This is looking for the object domain "ID".
-$domainid = $getdomaindata.domains.Where({ $_.name -eq $zone }).id
+    # This is looking for the object domain "ID".
+    $domainid = $getdomaindata.domains.Where({ $_.name -eq $zone }).id
 
-# Json data for Dynu API.
-$jsonbody = ConvertTo-Json @{
+    # Json data for Dynu API.
+    $jsonbody = ConvertTo-Json @{
 
-    domainId           = $domainid
-    domainName         = $id # _acme-challenge.yourdomain.com
-    nodeName           = "_acme-challenge"
-    recordType         = "TXT"
-    ttl                = "60"
-    state              = "true"
-    textData           = $value
+        domainId           = $domainid
+        domainName         = $id # _acme-challenge.yourdomain.com
+        nodeName           = "_acme-challenge"
+        recordType         = "TXT"
+        ttl                = "60"
+        state              = "true"
+        textData           = $value #HashValue from Let's Encrypt
 
-} # End Json data for Dynu API.
+}
 
-# POST TXT record to Dynu Record.
-Invoke-RestMethod -Method POST -Uri "https://api.dynu.com/v2/dns/$($domainid)/record" -ContentType ‘application/json’ -Headers @{ “Api-Key” = $APIKey } -Body $jsonbody
+    # POST TXT record to Dynu Record.
+    Invoke-RestMethod -Method POST -Uri "https://api.dynu.com/v2/dns/$($domainid)/record" -ContentType ‘application/json’ -Headers @{ “Api-Key” = $APIKey } -Body $jsonbody
 
-} # End "if"
+}
 else
 {
 
-Write-Host "API Key file not found. Please re-run or run 'Setup.ps1' as administrator"
-exit
+    Write-Host "API Key file not found. Please re-run or run 'Setup.ps1' as administrator"
+    exit
 
-} # End "else"
+}
