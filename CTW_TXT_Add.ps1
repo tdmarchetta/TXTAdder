bda64c347564cecd17c545bc4bf299a018291bc2
tdmarchetta <taylor@marchetta.me>
2021-12-03 04:11:02 -0800

    Change the stored API key

--- CTW_TXT_Add.ps1
+++ CTW_TXT_Add.ps1
@@ -1,6 +1,6 @@
 ﻿# TXTAdder
 # PowerShell To Adding TXT record with Certify the Web
-# Version 1.0.4
+# Version 1.0.5
 # Author: Taylor D. Marchetta
 # Huge thanks to Kyle Grey.
 
@@ -11,45 +11,45 @@ param (
     [string]$value  #HashValue from Let's Encrypt
     )
     
-# Force the use of TLS 1.2    
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    # Force the use of TLS 1.2    
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Check for 'C:\TXTAdder\DynuAPIKey.txt' file.
-if (Test-Path C:\TXTAdder\DynuAPIKey.txt) {
+    # Check for 'C:\TXTAdder\DynuAPIKey.txt' file.
+    if (Test-Path C:\TXTAdder) {
 
-    # Removes "IX HashValue" in wildcards.
-    $zone = $zone -replace "'\*.',"
+        # Removes "IX HashValue" in wildcards.
+        $zone = $zone -replace "'\*.',"
 
-    # Gets the API Key from the file "DynuAPIKey.txt".
-    $APIKey = Get-Content -Path 'C:\TXTAdder\DynuAPIKey.txt'
+        # Gets the API Key from the Windows Credentail Manager.
+        $APIKey = Get-StoredCredential -Target DynuAPIKey
 
-    # GET Request to Dynu.com - This will get the ID of the domain.
-    $getdomaindata = Invoke-RestMethod -Method GET -Uri ‘https://api.dynu.com/v2/dns/’ -ContentType ‘application/json’ -Headers @{ “Api-Key” = $APIKey }
+        # GET Request to Dynu.com - This will get the ID of the domain.
+        $getdomaindata = Invoke-RestMethod -Method GET -Uri ‘https://api.dynu.com/v2/dns/’ -ContentType ‘application/json’ -Headers @{ “Api-Key” = $APIKey }
 
-    # This is looking for the object domain "ID".
-    $domainid = $getdomaindata.domains.Where({ $_.name -eq $zone }).id
+        # This is looking for the object domain "ID".
+        $domainid = $getdomaindata.domains.Where({ $_.name -eq $zone }).id
 
-    # Json data for Dynu API.
-    $jsonbody = ConvertTo-Json @{
+        # Json data for Dynu API.
+        $jsonbody = ConvertTo-Json @{
 
-        domainId           = $domainid
-        domainName         = $id # _acme-challenge.yourdomain.com
-        nodeName           = "_acme-challenge"
-        recordType         = "TXT"
-        ttl                = "60"
-        state              = "true"
-        textData           = $value #HashValue from Let's Encrypt
+            domainId           = $domainid
+            domainName         = $id # _acme-challenge.yourdomain.com
+            nodeName           = "_acme-challenge"
+            recordType         = "TXT"
+            ttl                = "60"
+            state              = "true"
+            textData           = $value #HashValue from Let's Encrypt
 
-}
+    }
 
-    # POST TXT record to Dynu Record.
-    Invoke-RestMethod -Method POST -Uri "https://api.dynu.com/v2/dns/$($domainid)/record" -ContentType ‘application/json’ -Headers @{ “Api-Key” = $APIKey } -Body $jsonbody
+        # POST TXT record to Dynu Record.
+        Invoke-RestMethod -Method POST -Uri "https://api.dynu.com/v2/dns/$($domainid)/record" -ContentType ‘application/json’ -Headers @{ “Api-Key” = $APIKey } -Body $jsonbody
 
-}
-else
-{
+    }
+    else
+    {
 
-    Write-Host "API Key file not found. Please re-run or run 'Setup.ps1' as administrator"
-    exit
+        Write-Host "API Key file not found. Please re-run or run 'Setup.ps1' as administrator"
+        exit
 
-}
+    }
--- Setup.ps1
+++ Setup.ps1
@@ -1,10 +1,18 @@
 ﻿# PowerShell To Setting up Scripts to talk to Certify The Web & Dynu.
-# Version 1.0.6
+# Version 1.0.7
 # Author: Taylor D. Marchetta
 
 # Variables
 $currentlocation = Get-Location
 $TXTAdder = "C:\TXTAdder\"
+
+#Install Module
+Install-Module -Name CredentialManager
+Import-Module CredentialManager
+
+#Get API key and Stored in Credential Manager
+$DynuAPIKey = Read-Host -Prompt "Enter the API key"
+New-StoredCredential -Target DynuAPIKey -Persist LOCAL_MACHINE -Comment 'Dynu API Key for Certify the Web.' -Type Generic -Credentials $DynuAPIKey
 
 # Check for 'C:\TXTAdder' folder.
 # '.trim' will remove leading and trailing spaces and leading and trailing line breaks
@@ -16,10 +24,6 @@ if (!(test-path $TXTAdder.trim())) {
 
 # Copy file(s) from the location where "Setup.ps1" is at.
 Copy-Item -Path "$currentlocation\CTW_*" -Destination "$TXTAdder" -Recurse -Force
-
-# $getAPIKey | Add-Content -Path - PowerShell will ask for the Dynu API Key.
-$getAPIKey = Read-Host -Prompt 'Dynu API Key, Please'
-$getAPIKey | Add-Content -Path "$TXTAdder\DynuAPIKey.txt"
 
 # These PowerShell scripts are not signed...
 Set-ExecutionPolicy Bypass
